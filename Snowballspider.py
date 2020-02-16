8a5fd8138a90163e807ec93a0e75057b1b32be3c
cancan-huang <ruc_cancan@ruc.edu.cn>
2020-02-16 16:53:04 +0000

    1

---
+++ Snowballspider.py
@@ -0,0 +1,18 @@
+# Snowball Finance Spider
+'''
+from crawler.spiders import util
+from scrapy.spiders import Spider
+from datetime import datetime
+from scrapy import Request
+from scrapy.utils.request import request_fingerprint
+from crawler.items import XQItem
+from crawler.settings import *
+import json
+import time
+import re
+
+class UserGuanzhu(Spider):
+    start_at = datetime.now()
+    name = 'xq_user_follow_updt'
+    logger = util.set_logger(name, )
+'''
